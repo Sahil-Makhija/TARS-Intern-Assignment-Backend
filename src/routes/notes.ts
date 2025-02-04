6b8d8e76643015984c66099bcc74528e9952e664
Sahil-Makhija <sahilmakhija667@gmail.com>
2025-02-05 02:41:08 +0530

    Notes CRUD

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -8,20 +8,69 @@ type Variables = JwtVariables;
 
 export const notes = new Hono<{ Variables: Variables }>();
 
-notes.use("/", jwt({ secret: process.env.JWT_SECRET! }));
+notes.use("/*", jwt({ secret: process.env.JWT_SECRET! }));
 
 notes.get("/", async (c) => {
   const userId = c.get("jwtPayload").id;
   const notes = await Note.find({ userId });
   if (!notes) {
-    return c.json({ error: "No notes found" });
+    return c.json({ status: false, error: "No notes found for this user!" });
   }
-  return c.json({ notes });
+  return c.json({ status: true, notes });
 });
 
 notes.post("/create", async (c) => {
-  const userId = c.get("jwtPayload").id;
-  const { title, content, images } = await c.req.json();
-  const note = await Note.create({ userId, title, content, images });
+  const { userId } = c.get("jwtPayload");
+  const { title, content, images, audioTranscription } = await c.req.json();
+  const note = await Note.create({
+    userId,
+    title,
+    content,
+    images,
+    audioTranscription,
+  });
+  return c.json({ note });
+});
+
+notes.delete("/delete", async (c) => {
+  const { id: userId } = c.get("jwtPayload");
+  const { noteId } = await c.req.json();
+  const note = await Note.findOneAndDelete({ _id: noteId, userId });
+  if (!note) {
+    return c.json({
+      status: false,
+      error: "Note not found or you're not authorized to delete this note!",
+    });
+  }
+  return c.json({ status: true, message: "Note deleted successfully!" });
+});
+
+notes.get("/:noteId", async (c) => {
+  const { id: userId } = c.get("jwtPayload");
+  const { noteId } = c.req.param();
+  const note = await Note.findOne({ _id: noteId, userId });
+  if (!note) {
+    return c.json({
+      status: false,
+      error: "Note not found or you're not authorized to delete this note!",
+    });
+  }
+  return c.json({ note });
+});
+
+notes.get("/:noteId/favourite", async (c) => {
+  const { id: userId } = c.get("jwtPayload");
+  const { noteId } = c.req.param();
+  const note = await Note.findOne({ _id: noteId, userId });
+  if (note) {
+    note.isFavorite = !note.isFavorite;
+    await note.save();
+  }
+  if (!note) {
+    return c.json({
+      status: false,
+      error: "Note not found or you're not authorized to delete this note!",
+    });
+  }
   return c.json({ note });
 });
